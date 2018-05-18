9e6bc9f6a3ac67d02c2c5ab80bf36858b7ce5d03
Tasya Aditya Rukmana <tadityar@gmail.com>
2018-05-18 13:14:05 +0800

    HW 1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/arms18/groupx_ws/src
+# Build directory: /home/arms18/groupx_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(learning_communication)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/arms18/groupx_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/arms18/groupx_ws/devel;/home/arms18/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/arms18/groupx_ws/devel;/home/arms18/catkin_ws/devel;/opt/ros/indigo" != "" else []
